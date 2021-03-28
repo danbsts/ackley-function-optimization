b2fad67ad07a028ba6ea53dfe6bcb00bdd342149
Daniel <dmrb2@cin.ufpe.br>
2021-03-28 20:44:59 -0300

    Implement constant for standard deviation calculation

--- main_2.py
+++ main_2.py
@@ -2,6 +2,11 @@ import random
 import math
 import numpy as np
 
+successful_mutations = 0
+total_mutations = 0
+todo = 'divide'
+deviation_constant = 0.8
+
 def square_sum(vetor):
   sum = 0
   for x in vetor:
@@ -14,25 +19,52 @@ def cos_sum(vetor):
     sum += math.cos(math.pi*2*x)
   return sum
 
-def calculate_fitness(population):
+def calculate_population_fitness(population):
   result = []
-  for x,desvio in population:
-    term1 = (-20.0)* math.exp( (-0.2) *math.sqrt(square_sum(x)/30.0) )
-    term2 = -math.exp(cos_sum(x)/30.0)
-    y =  term2 + math.exp(1) +term1 + 20.0 
-    result.append((x,desvio,y))
+  for i in population:
+    result.append(calculate_fitness(i))
   return result
 
-def mutate(population):
+def calculate_fitness(individual):
+  (x, desvio) = individual
+  term1 = (-20)* math.exp( (-0.2) *math.sqrt(square_sum(x)/len(x)) )
+  term2 = -math.exp(cos_sum(x)/len(x))
+  y =  term2 + math.exp(1) +term1 + 20.0 
+  return (x,desvio,y)
+
+def get_constant():
+  global total_mutations, successful_mutations, todo
+  if total_mutations > 0 and successful_mutations / total_mutations < 0.2:
+    todo = 'multiply'
+  elif total_mutations > 0 and successful_mutations / total_mutations > 0.2:
+    todo = 'divide'
+  else:
+    todo = 'keep'
+
+def mutate(population): #1.899744051
+  global todo, deviation_constant, successful_mutations, total_mutations
   new_population = []
+  get_constant()
+  successful_mutations = 0
+  total_mutations = 0
   while(len(new_population) < 7 * len(population)):
     random_idx = random.randint(0, len(population) - 1)
     (parent_feature, std_deviation, parent_fitness) = population[random_idx]
+    std_deviation = max(std_deviation, 1.55e-1)
     new_std_deviation =  abs(std_deviation * math.exp((1/pow(len(parent_feature), 0.5)) * np.random.normal(0, 1)))
+    if todo == 'divide':
+      new_std_deviation /= deviation_constant
+    elif todo == 'multiply':
+      new_std_deviation *= deviation_constant
     child_feature = []
     for i in range(len(parent_feature)):
-      child_feature.append(max(min(parent_feature[i] + np.random.normal(0, new_std_deviation), 15), -15))
-    new_population.append((child_feature, new_std_deviation))
+        child_feature.append(max(min(parent_feature[i] + np.random.normal(0, new_std_deviation), 15), -15))
+    child = (child_feature, new_std_deviation)
+    child_fitness = calculate_fitness(child)
+    if child_fitness[2] < parent_fitness:
+      successful_mutations += 1
+    total_mutations += 1
+    new_population.append(child)
   return new_population
 
 def parent_selection(population):
@@ -64,19 +96,21 @@ def eval(population_fitness):
     return None
 
 def ackley_function_optimization():
+  global todo, total_mutations, successful_mutations
   population_size = 20
   iterations = 10000
   population = init_population(population_size)
-  population_fitness = calculate_fitness(population)
+  population_fitness = calculate_population_fitness(population)
   solution = eval(population_fitness)
   count = 0
   while solution == None and count < iterations:
       children = mutate(population_fitness)
-      children_fitness = calculate_fitness(children)
+      children_fitness = calculate_population_fitness(children)
       population_fitness = survival_selection(children_fitness, population_size)
       solution = eval(population_fitness)
-      if count % 100 == 0: print(count, '->', population_fitness[0][2], 'dis->',population_fitness[0][1])
+      print(count, '->', population_fitness[0][2], todo, '--', successful_mutations/total_mutations, 'dis->',population_fitness[0][1])
       count += 1
+      if count % 30 == 0: print("\033[H\033[J")
   if count == iterations:
       return (-1, 0, 1000, 0)
   else:
